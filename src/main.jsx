3498c28c4aedc24dde2e35ce0c8ad45baea36044
Vanessa Henao <lvhenao0@gmail.com>
2025-11-12 20:21:47 -0500

    Clase_6_Agenda_ADSO_v4_Tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
-// 🧩 Importamos React, el corazón de nuestra app
 import React from 'react'
-
-// 🛠️ Importamos ReactDOM para conectar React con el navegador
 import ReactDOM from 'react-dom/client'
-
-// 📒 Importamos el componente principal de la aplicación
 import App from './App.jsx'
+import './index.css'   // <--- IMPORTANTE
 
-// 🎨 Importamos los estilos globales
-import './index.css'
-
-// 🚀 Aquí es donde todo comienza: renderizamos la app dentro del elemento con id "root"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* 🧠 StrictMode nos ayuda a detectar errores y buenas prácticas durante el desarrollo */}
     <App />
   </React.StrictMode>,
 )
